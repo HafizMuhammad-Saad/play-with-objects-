5c783576487a2abd6c6597445853adfcf0d887e6
HafizMuhammad-Saad <studentsaad41@gmail.com>
2024-12-05 16:58:14 +0500

    update

--- app.js
+++ app.js
@@ -1,99 +1,138 @@
-const gadgets = {
-    products: [
-      { name: "Smartphone", price: 1100 },
-      { name: "Smartwatch", price: 400 },
-      { name: "Laptop", price: 1500 },
-      { name: "Tablet", price: 700 },
-      { name: "glass", price: 700 },
-      { name: "cup", price: 1700 },
-      { name: "cofin", price: 100 },
-    ]
-  };
+// Modal Login and SignUp
 
-  function highLowPrice() {
-      gadgets.products.forEach((product) => {
-        console.log(product.name, product.price);
-    var container = document.querySelector('.main')
+var email = document.getElementById('email1');
+var password = document.getElementById('password1');
+
+var emailValue = 'saad123@gmail.com';
+var passwordValue = '123'
+
+
+function show() {
+  if (email.value === emailValue && password.value === passwordValue) {
+    console.log(email.value);
+    console.log(password.value);
+    
+  } else {
+    alert('Please enter a valid email address and password ');
+  }
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ---------------------------------------------------------------------------------------
+
+// const gadgets = {
+//     products: [
+//       { name: "Smartphone", price: 1100 },
+//       { name: "Smartwatch", price: 400 },
+//       { name: "Laptop", price: 1500 },
+//       { name: "Tablet", price: 700 },
+//       { name: "glass", price: 700 },
+//       { name: "cup", price: 1700 },
+//       { name: "cofin", price: 100 },
+//     ]
+//   };
+
+//   function highLowPrice() {
+//       gadgets.products.forEach((product) => {
+//         console.log(product.name, product.price);
+//     var container = document.querySelector('.main')
         
     
-        if (product.price > 1000) {
-            container.innerHTML += `<h4 class="crimson text-light p-2 rounded ">${product.name } is a expensive product with a price of ${product.price}</h4>`;
-       } if (product.price < 1000) {
-           container.innerHTML += `<h4 class="forestgreen text-light p-2 rounded ">${product.name } is a low price product with a price of ${product.price}</h4>`;
-       }
-      })
+//         if (product.price > 1000) {
+//             container.innerHTML += `<h4 class="crimson text-light p-2 rounded ">${product.name } is a expensive product with a price of ${product.price}</h4>`;
+//        } if (product.price < 1000) {
+//            container.innerHTML += `<h4 class="forestgreen text-light p-2 rounded ">${product.name } is a low price product with a price of ${product.price}</h4>`;
+//        }
+//       })
     
-  }
+//   }
 
-  highLowPrice()
-  var gadgets1 = {
-    product1: { smartWatch: { name: "Smartwatch", price: 1999 }, },
-    product2: { smartPhone : { name: "Smartphone", price: 1699 } },
-    product3: { laptop : { name: "Laptop", price: 599 } } ,
-    product4: { tablet : { name: "Tablet", price: 199 } },
-  }
+//   highLowPrice()
+//   var gadgets1 = {
+//     product1: { smartWatch: { name: "Smartwatch", price: 1999 }, },
+//     product2: { smartPhone : { name: "Smartphone", price: 1699 } },
+//     product3: { laptop : { name: "Laptop", price: 599 } } ,
+//     product4: { tablet : { name: "Tablet", price: 199 } },
+//   }
 
 
 
-  function expensiveProduct() {
-    var container = document.querySelector('.main')
-      for (var products in gadgets1) {
-        for (var product in gadgets1[products]) {
-            var details = gadgets1[products][product];
-            if (details.price > 1000) {
-                 container.innerHTML += `<h4 class="crimson text-light p-2 rounded bg-gradient">${details.name } is a expensive product with a price of ${details.price}</h4>`;
-            } if (details.price < 1000) {
-                container.innerHTML += `<h4 class="forestgreen text-light p-2 rounded bg-gradient">${details.name } is a medium price product with a price of ${details.price}</h4>`;
-            }
+//   function expensiveProduct() {
+//     var container = document.querySelector('.main')
+//       for (var products in gadgets1) {
+//         for (var product in gadgets1[products]) {
+//             var details = gadgets1[products][product];
+//             if (details.price > 1000) {
+//                  container.innerHTML += `<h4 class="crimson text-light p-2 rounded bg-gradient">${details.name } is a expensive product with a price of ${details.price}</h4>`;
+//             } if (details.price < 1000) {
+//                 container.innerHTML += `<h4 class="forestgreen text-light p-2 rounded bg-gradient">${details.name } is a medium price product with a price of ${details.price}</h4>`;
+//             }
             
-      }
-    }
+//       }
+//     }
     
-  }
+//   }
 //   expensiveProduct()
 
 
 
-const warehouse = {
-    categories: {
-      furniture: [
-        { name: "Sofa", price: 700 },
-        { name: "Table", price: 300 },
-      ],
-      electronics: [
-        { name: "Speaker", price: 100 },
-        { name: "Monitor", price: 200 },
-      ],
-      mobiles: [
-        { name: "Speaker", price: 100 },
-        { name: "Monitor", price: 200 },
-        { name: "Speaker", price: 100 },
-        { name: "Monitor", price: 200 },
-      ],
-    },
-  };
+// const warehouse = {
+//     categories: {
+//       furniture: [
+//         { name: "Sofa", price: 700 },
+//         { name: "Table", price: 300 },
+//       ],
+//       electronics: [
+//         { name: "Speaker", price: 100 },
+//         { name: "Monitor", price: 200 },
+//       ],
+//       mobiles: [
+//         { name: "Speaker", price: 100 },
+//         { name: "Monitor", price: 200 },
+//         { name: "Speaker", price: 100 },
+//         { name: "Monitor", price: 200 },
+//       ],
+//     },
+//   };
 
-  function countsProductInCategory() {
-    var data = '';
-    var cate = warehouse.categories;
-    Object.keys(cate)
-    // console.log(cate);
-    for(let category in cate){
-    //   console.log(`Category: ${category}, Number of items: ${cate[category].length}`);
+//   function countsProductInCategory() {
+//     var data = '';
+//     var cate = warehouse.categories;
+//     Object.keys(cate)
+//     // console.log(cate);
+//     for(let category in cate){
+//     //   console.log(`Category: ${category}, Number of items: ${cate[category].length}`);
 
 
-       data += `
-        <p class="h4 py-2">
-         Category: <span class="text-secondary bg-light"> ${category} </span>
-         , Number of items: <span class="text-success bg-light"> ${cate[category].length} </span>
-        </p>
-      `
-    }
+//        data += `
+//         <p class="h4 py-2">
+//          Category: <span class="text-secondary bg-light"> ${category} </span>
+//          , Number of items: <span class="text-success bg-light"> ${cate[category].length} </span>
+//         </p>
+//       `
+//     }
    
-    document.querySelector('.container').innerHTML = data;
+//     document.querySelector('.container').innerHTML = data;
     
-  }
-  countsProductInCategory();
+//   }
+//   countsProductInCategory();
 
 
 
